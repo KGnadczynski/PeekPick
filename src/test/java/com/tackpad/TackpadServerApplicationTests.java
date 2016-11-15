35e00d8686fb6de052ff1330f379e40ed4d99cf2
Wojciech Grazawski <wojciech.grazawski@wp.pl>
2016-11-15 21:14:14 +0100

    Initial commit

---
+++ src/test/java/com/tackpad/TackpadServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tackpad;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TackpadServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
