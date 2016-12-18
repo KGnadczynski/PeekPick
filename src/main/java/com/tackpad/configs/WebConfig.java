777e098d1e0716f3c920ded7ecbe972bf04cd1a2
Wojciech Grazawski <wojciech.grazawski@wp.pl>
2016-12-18 19:12:51 +0100

    Enable cors

---
+++ src/main/java/com/tackpad/configs/WebConfig.java
@@ -0,0 +1,19 @@
+package com.tackpad.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Wojtek on 2016-12-18.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
