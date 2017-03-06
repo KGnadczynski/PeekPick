c495190697eace4eb215ad7a3ca6c96a198bd5fe
Your Name <you@example.com>
2017-03-06 10:22:32 +0100

    Dodanie swaggera

--- src/main/java/com/tackpad/configs/WebConfig.java
+++ src/main/java/com/tackpad/configs/WebConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * Created by Wojtek on 2016-12-18.
  */
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
