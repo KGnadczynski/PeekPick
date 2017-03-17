07f3e0bbf0ed755786660c4dfae03fa7a352260a
Przemysław Żynis <p.zynis@volanto.pl>
2017-03-17 14:37:36 +0100

    usuwanie postów

--- src/main/java/com/tackpad/dao/MessageDaoImpl.java
+++ src/main/java/com/tackpad/dao/MessageDaoImpl.java
@@ -127,7 +127,8 @@ public class MessageDaoImpl extends BaseDaoImpl<Message> implements MessageDao {
 			sql.append(" and COM.name LIKE :searchTerm OR COM_BRA.name LIKE :searchTerm OR MES.content LIKE :searchTerm ");
 		}
 
-		sql.append(" and NOT (MES.status <=> 'DELETED') ");
+		sql.append(" and NOT (MES.status = 'DELETED') ");
+
 		sql.append(" GROUP BY MESS_COM_BRA.Message_id ");
 
 		if (range != null) {
