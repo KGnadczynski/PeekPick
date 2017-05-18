2698c10f849e763af6415c04d2ee488edf1c24cd
Your Name <you@example.com>
2017-05-18 21:40:00 +0200

    odnow

--- src/main/java/com/tackpad/scheduledtasks/RemoveNotSendUserNotifications.java
+++ src/main/java/com/tackpad/scheduledtasks/RemoveNotSendUserNotifications.java
@@ -7,6 +7,8 @@ import com.tackpad.services.SendFCMRequestsService;
 import com.tackpad.services.UserDeviceFCMTokenService;
 import com.tackpad.services.UserNotificationService;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -16,6 +18,8 @@ import java.util.List;
 @Component
 public class RemoveNotSendUserNotifications {
 
+    private static final Logger log = LoggerFactory.getLogger(RemoveEndedPosts.class);
+
     private static final int EXPIRED_DAY_COUNT = 3;
 
     @Autowired
@@ -24,6 +28,7 @@ public class RemoveNotSendUserNotifications {
     //3h
     @Scheduled(fixedRate = 3*60*60*1000 )
     public void sendFCM() {
+        log.info(" ADD FREE PLANS TASK");
         List<UserNotification> userNotificationList = userNotificationService.gatListByStatus(UserNotificationStatus.NOT_SEND);
         for (UserNotification userNotification : userNotificationList) {
             if (userNotification.getCreateDate().after(DateTime.now().plusDays(EXPIRED_DAY_COUNT).toDate())) {
